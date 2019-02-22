7746764f54a0031ffee340b472dec1370d4f462f
Alex <ezyah3@nottingham.ac.uk>
2019-02-22 17:22:52 +0000

    test

--- index.js
+++ index.js
@@ -17,13 +17,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var numUsers = 0;
 
-
-io.on('connection',function(socket){
+io.on('connection', (socket) => {
   var addedUser = false;
-  
-    console.log('one user connected '+socket.id);
 
- // when the client emits 'add user', this listens and executes
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
   socket.on('add user', (username) => {
     if (addedUser) return;
 
@@ -41,15 +47,30 @@ io.on('connection',function(socket){
     });
   });
 
-
-    socket.on('CHAT' , function (data) {
-        console.log('======CHAT message========== ');
-        console.log(data);
-        socket.emit('CHAT',data);
-
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
     });
+  });
 
-    socket.on('disconnect',function(){
-        console.log('one user disconnected '+socket.id);
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
     });
-})
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
