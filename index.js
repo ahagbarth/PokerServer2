1aebbb9a5b112d3202022e01343583ac389652fd
Alex <ezyah3@nottingham.ac.uk>
2019-02-22 14:37:39 +0000

    update

--- index.js
+++ index.js
@@ -18,10 +18,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 var numUsers = 0;
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  var addedUser = false;
 
-  socket.emit('hello', "you are connected");
-  //socket.on('')
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', (username) => {
@@ -41,7 +47,30 @@ io.on('connection', (socket) => {
     });
   });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
