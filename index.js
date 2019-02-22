682e94777dfd8294c3d698923d09eb7dd813f6f3
Alex <ezyah3@nottingham.ac.uk>
2019-02-22 14:30:07 +0000

    update

--- index.js
+++ index.js
@@ -23,7 +23,23 @@ io.on('connection', (socket) => {
   socket.emit('hello', "you are connected");
   //socket.on('')
 
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
 
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
