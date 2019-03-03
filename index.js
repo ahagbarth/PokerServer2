174fd841ce3047b4a6c34b012da3502567876a50
Alex <ezyah3@nottingham.ac.uk>
2019-03-03 22:13:59 +0000

    test

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ io.on('connection', (socket) => {
   // when the client emits 'add user', this listens and executes
   socket.on('add user', (username) => {
     if (addedUser) return;
-    
+
     socket.join("Room 1");
     // we store the username in the socket session for this client
     socket.username = username;
@@ -94,7 +94,7 @@ if(numUsers > 5) {
       waitingList: waitingList
     });
     // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
+    socket.broadcast.to('Room 1').emit('user joined', {
       username: socket.username,
       numUsers: numUsers,
       users: users
