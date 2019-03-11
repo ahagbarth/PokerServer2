ee1712ef5b3335ed554951d5074dd1ad95b227f8
Alex <ezyah3@nottingham.ac.uk>
2019-03-11 19:56:25 +0000

    test

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ io.on('connection', (socket) => {
 
    socket.on('pass_turn', {
     
-    if(numUsers > 1) {
+    if(numUsers > "1") {
 
         if(users.indexOf(socket.username) == turnState) {
           socket.emit('passTurn', {
