5e57b562c64dfed5a36404b998d3c598483e2782
Alex <ezyah3@nottingham.ac.uk>
2019-04-22 06:31:31 +0100

    revert

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Chatroom
 
-
+var users = [];
+var waitingList = [];
+var numUsers = 0;
+var userPosition = 0; 
 
 
 var cardDeck = deck.createPack();
@@ -32,10 +35,8 @@ var userHand;
 var userHandCompare;
 
 
+var tableState;
 
-var waitingList = [];
-
-var userPosition = 0; 
 
 //////////////////
     var turnState =0 ;
@@ -53,7 +54,46 @@ io.on('connection', (socket) => {
   */
 
 
-  
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    
+    
+    if(numUsers > 5) {
+      tableState = "unavailable";
+
+
+      waitingList.push(socket.username);
+    } else {
+      tableState = "available";
+      users.push(socket.username);
+      userPosition = users.indexOf(socket.username);
+    }
+
+
+    
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers,
+      tableState: tableState,
+      users: users,
+      waitingList: waitingList,
+      userPosition: userPosition
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers,
+      users: users
+    });
+
+
+
+  });
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', () => {
@@ -102,52 +142,6 @@ socket.on("roomName", ()=>{
       var maxRoundBet = 0;
       var usersFold = [];
       var usersStillPlaying = [];
-      var users = [];
-      var numUsers = users.length();
-
-      
-      
-      var tableState;
-      // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    
-    
-    if(numUsers > 5) {
-      tableState = "unavailable";
-
-
-      waitingList.push(socket.username);
-    } else {
-      tableState = "available";
-      users.push(socket.username);
-      userPosition = users.indexOf(socket.username);
-    }
-
-
-    
-    //++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers,
-      tableState: tableState,
-      users: users,
-      waitingList: waitingList,
-      userPosition: userPosition
-    });
-    // echo globally (all clients) that a person has connected
-    socket.to(roomName).emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers,
-      users: users
-    });
-
-
-
-  });
   
       socket.on('fold', (data)=>{
         usersFold.push(socket.username);
